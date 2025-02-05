f6afba0835116d7d5a8cf3b0dc41b3e688eda137
mdrezaulkarim38 <mdrezaulkarim31295@gmail.com>
2025-02-05 17:01:36 +0600

    Now Bread Crumb is working

--- client/src/pages/users/HealthInsurance.tsx
+++ client/src/pages/users/HealthInsurance.tsx
@@ -1,3 +1,4 @@
+import Breadcrumb from "../../components/users/Breadcrumb";
 import Footer from "../../components/users/Footer";
 import Header from "../../components/users/Header";
 
@@ -5,6 +6,7 @@ function HealthInsurance(){
     return (
         <div className="main-wrapper">
             <Header />
+            <Breadcrumb pageName="Health Insurance" />
             <h2>This is Health Insurance Page</h2>
             <Footer />
         </div>
