0a7ed7da4cca318d0f7cc101115b5df19ce55c67
mdrezaulkarim38 <mdrezaulkarim31295@gmail.com>
2025-02-05 11:17:05 +0600

    All Bootstrap added by npm

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
-
+import "./assets/users/assets/css/style.css"
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import Home from "./pages/users/Home";
 
 function App(){
--- client/src/pages/users/Home.tsx
+++ client/src/pages/users/Home.tsx
@@ -3,11 +3,33 @@ import Header from "../../components/users/Header";
 
 function Home(){
     return (
-        <>
+        <div className="main-wrapper">
         <Header />
-        <h3>This is Home Page</h3>
+        <section className="section section-search">
+				<div className="container-fluid">
+					<div className="banner-wrapper">
+						<div className="banner-header text-center">
+							<h1>Search Doctor, Make an Appointment</h1>
+							<p>Discover the best doctors, clinic & hospital the city nearest to you.</p>
+						</div>
+						<div className="search-box">
+							<form action="templateshub.net">
+								<div className="form-group search-location">
+									<input type="text" className="form-control" placeholder="Search Location" />
+									<span className="form-text">Based on your Location</span>
+								</div>
+								<div className="form-group search-info">
+									<input type="text" className="form-control" placeholder="Search Doctors, Clinics, Hospitals, Diseases Etc" />
+									<span className="form-text">Ex : Dental or Sugar Check up etc</span>
+								</div>
+								<button type="submit" className="btn btn-primary search-btn"><i className="fas fa-search"></i> <span>Search</span></button>
+							</form>
+						</div>
+					</div>
+				</div>
+			</section>
         <Footer />
-        </>
+        </div>
     );
 }
 
