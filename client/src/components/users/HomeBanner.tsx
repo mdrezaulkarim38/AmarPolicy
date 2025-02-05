508b6c0dbf2f9d78f80caddc8e7c38d5cd004a1a
mdrezaulkarim38 <mdrezaulkarim31295@gmail.com>
2025-02-05 16:04:57 +0600

    Now need to work with others

--- client/src/components/users/HomeBanner.tsx
+++ client/src/components/users/HomeBanner.tsx
@@ -1,36 +1,31 @@
 function HomeBanner(){
     return (
-        <>
-        <section className="section section-search">
-        <div className="container-fluid">
-          <div className="banner-wrapper">
-            <div className="banner-header text-center">
-              <h1>Search Doctor, Make an Appointment</h1>
-              <p>Discover the best doctors, clinic & hospital the city nearest to you.</p>
-            </div>
-            <div className="search-box">
-              <form action="#">
-                <div className="form-group search-location">
-                  <input type="text" className="form-control" placeholder="Search Location" />
-                  <span className="form-text">Based on your Location</span>
-                </div>
-                <div className="form-group search-info">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Search Doctors, Clinics, Hospitals, Diseases Etc"
-                  />
-                  <span className="form-text">Ex: Dental or Sugar Check-up etc</span>
-                </div>
-                <button type="submit" className="btn btn-primary search-btn">
-                  <i className="fas fa-search"></i> <span>Search</span>
-                </button>
-              </form>
-            </div>
+      <section className="section section-search">
+      <div className="container-fluid">
+        <div className="banner-wrapper">
+          <div className="banner-header text-center">
+            <h1>Simply compare, order, and covered.</h1>
+            <p>Reliable digital insurance solutions for you.</p>
+          </div>
+
+          <div className="search-box">
+            <form>
+              <div className="form-group search-location">
+                <input type="text" className="form-control" placeholder="Search Insurance" />
+                <span className="form-text">Based on your Choice</span>
+              </div>
+              <div className="form-group search-info">
+                <input type="text" className="form-control" placeholder="Search by Insurance Name you are looking about" />
+                <span className="form-text">Ex : Life insurance or Health etc</span>
+              </div>
+              <button type="submit" className="btn btn-primary search-btn">
+                <i className="fas fa-search"></i> <span>Search</span>
+              </button>
+            </form>
           </div>
         </div>
-      </section>
-        </>
+      </div>
+    </section>
     );
 }
 
