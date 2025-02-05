f5b33aed71488a410ed793cfeba45d13f261a5a6
mdrezaulkarim38 <mdrezaulkarim31295@gmail.com>
2025-02-05 16:35:32 +0600

    Add new Page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,15 @@
-import "./assets/users/assets/css/style.css"
+import { Route, Routes } from "react-router-dom";
+import "./assets/users/assets/css/style.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import Home from "./pages/users/Home";
+import HealthInsurance from "./pages/users/HealthInsurance";
 
-function App(){
+function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/health-insurance" element={<HealthInsurance />} />
+    </Routes>
   );
 }
 
---
+++ client/src/pages/users/HealthInsurance.tsx
@@ -0,0 +1,14 @@
+import Footer from "../../components/users/Footer";
+import Header from "../../components/users/Header";
+
+function HealthInsurance(){
+    return (
+        <div className="main-wrapper">
+            <Header />
+            <h2>This is Health Insurance Page</h2>
+            <Footer />
+        </div>
+    );
+}
+
+export default HealthInsurance;
