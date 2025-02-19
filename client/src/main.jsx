e7511fc811b3f364d82577bc684d33897f9027ae
mdrezaulkarim38 <mdrezaulkarim31295@gmail.com>
2025-02-19 17:28:09 +0600

    only asset css I use

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css"
+import './assets/users/assets/css/bootstrap.min.css'
 import './assets/users/assets/js/jquery.min.js'
-import './assets/users/assets/css/style.css'
 import './assets/users/assets/js/script.js'
 import './index.css'
-
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
